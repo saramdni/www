05bedd4854579be43b112c2de22a89cdb0f3c04b
Seyedeh Sara Madani <S.Madani@FST.NET>
2024-11-30 15:43:44 +0330

    this is an initial to start working of my website

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+/*eslint @typescript-eslint/no-unsafe-assignment:0 */
+
+/** @type {import("prettier").Config} */
+const config = {
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+}
+
+module.exports = config
