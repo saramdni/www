8445a4eb1a57428d0e996e3a6c6f465345a2f4b5
Sara Madani <madnaisara30@gmail.com>
2025-08-21 00:10:42 +0330

    relocate page.tsx and adjust imports

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -1,28 +1,23 @@
-import Link from 'next/link'
-import { useTranslation } from '../i18n'
-import Header from '@/components/common/header'
+import Navbar from '@/components/common/Navbar'
+import Hero from '@/components/Hero'
+import About from '@/components/About'
+import Experience from '@/components/Experience'
+import Services from '@/components/Services'
+import Projects from '@/components/Projects/Projects'
+import Contact from '@/components/Contact'
 
-type HomeProps = {
-  params: {
-    lng: string
-  }
-}
-
-export async function generateStaticParams() {
-  const supportedLanguages = ['en', 'fa']
-  return supportedLanguages.map((lng) => ({ lng }))
-}
-
-export default async function Home({ params }: HomeProps) {
-  const lng = params?.lng // Destructure params synchronously
-  const { t } = await useTranslation(lng)
-
+export default function Home({ params: { lng } }: { params: { lng: string } }) {
   return (
-    <div className="grid mt-8 grid-rows-[16px_1fr_16px] justify-items-center min-h-screen gap-16">
-      <Header params={{ lng }} />
-      <h1>Hi, I am Sara</h1>
-      <h1>{t('title')}</h1>
-      <Link href={`/${lng}/about`}>{t('to-second-page')}</Link>
-    </div>
+    <>
+      <Navbar lng={lng} />
+      <main className="container">
+        <Hero />
+        <About />
+        <Experience />
+        <Services />
+        <Projects />
+        <Contact />
+      </main>
+    </>
   )
 }
